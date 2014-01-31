c04a363bf589e887fe474253df1b33524f59bd16
Andreas <andreas.koell@uni-ulm.de>
2014-01-31 10:38:43 +0100

    initial project creation

---
+++ src/de/uulm/mi/mind_android/MainActivity.java
@@ -0,0 +1,15 @@
+package de.uulm.mi.mind_android;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}

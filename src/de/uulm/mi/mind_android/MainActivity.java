1c4d8368d18584240ef8652b68d6989e04f8d4ea
Tamino Hartmann <tamino.hartmann@gmail.com>
2014-02-04 15:14:55 +0100

    write & read db working

--- src/de/uulm/mi/mind_android/MainActivity.java
+++ src/de/uulm/mi/mind_android/MainActivity.java
@@ -1,13 +1,19 @@
 package de.uulm.mi.mind_android;
 
 import android.app.Activity;
-import android.content.*;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import de.uulm.mi.mind_android.data.WifiInfo;
+import de.uulm.mi.mind_android.database.Database;
 
 import java.util.ArrayList;
 
@@ -15,6 +21,7 @@ public class MainActivity extends Activity {
 
     private TextView mainText;
     private final String TAG = "MIND";
+    private ArrayList<WifiInfo> wifiList;
 
     /**
      * Called when the activity is first created.
@@ -27,12 +34,26 @@ public class MainActivity extends Activity {
         // button to force location
         Button forceB = (Button) findViewById(R.id.forceButton);
         forceB.setOnClickListener(new View.OnClickListener() {
-			
-			@Override
-			public void onClick(View v) {
-				// TODO implement onClick
-			}
-		});
+
+            private SQLiteDatabase database;
+
+            @Override
+            public void onClick(View v) {
+                Database helper = new Database(getApplicationContext());
+                database = helper.getWritableDatabase();
+
+                for (WifiInfo chunck : wifiList) {
+                    database.execSQL("INSERT INTO wifiInfos (ssid, mac, level) VALUES ('" + chunck.SSID + "', '" + chunck.MAC + "', '" + chunck.LEVEL + "')");
+                }
+
+                Cursor cur = database.query("wifiInfos", null, null, null, null, null, null);
+
+                // TODO only for debugging, values also need to be deleted sometimes :P
+                while (cur.moveToNext()) {
+                    Log.d(TAG+"|DEBUG", "" + cur.getString(1));
+                }
+            }
+        });
     }
 
     private void init() {
@@ -43,27 +64,19 @@ public class MainActivity extends Activity {
     }
 
     // Broadcast receiver for receiving status updates from the IntentService
-    private class ResponseReceiver extends BroadcastReceiver
-    {
+    private class ResponseReceiver extends BroadcastReceiver {
+
         // Prevents instantiation
         private ResponseReceiver() {
         }
+
         // Called when the BroadcastReceiver gets an Intent it's registered to receive
         public void onReceive(Context context, Intent intent) {
             ArrayList<WifiInfo> infolist = intent.getParcelableArrayListExtra("info");
 
             Log.d(TAG, "Activity received WifiInfo Intent");
 
-            StringBuilder sb = new StringBuilder();
-            for (WifiInfo w : infolist){
-                sb.append(w.SSID);
-                sb.append('|');
-                sb.append(w.MAC);
-                sb.append('|');
-                sb.append(w.LEVEL);
-                sb.append('\n');
-            }
-            mainText.setText(sb.toString());
+            wifiList = infolist;
         }
     }
 }
